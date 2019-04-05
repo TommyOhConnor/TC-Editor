307e6c1ae053f6a589cbb04d27589bb8e2964ebe
Tommyohconnor <me@tommy-oconnor.com>
2019-04-05 14:14:27 -0700

    forgot to add all files

---
+++ code/Editor.tsx
@@ -0,0 +1,55 @@
+import * as React from "react";
+import { PropertyControls, ControlType } from "framer";
+import { Editor } from "slate-react";
+import { Value } from "slate";
+
+// Create our initial value...
+const initialValue = Value.fromJSON({
+  document: {
+    nodes: [
+      {
+        object: "block",
+        type: "paragraph",
+        nodes: [
+          {
+            object: "text",
+            leaves: [
+              {
+                text: "A line of text in a paragraph."
+              }
+            ]
+          }
+        ]
+      }
+    ]
+  }
+});
+
+export class Editor extends React.Component<Props> {
+  // Set the initial value when the app is first constructed.
+  state = {
+    value: initialValue
+  };
+
+  // On change, update the app's React state with the new editor value.
+  onChange = ({ value }) => {
+    this.setState({ value });
+  };
+
+  // Render the editor.
+  render() {
+    return <Editor value={this.state.value} onChange={this.onChange} />;
+  }
+}
+
+// Define some standard CSS for your component
+const style: React.CSSProperties = {
+  height: "100%",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+  textAlign: "center",
+  color: "#8855FF",
+  background: "rgba(136, 85, 255, 0.1)",
+  overflow: "hidden"
+};
